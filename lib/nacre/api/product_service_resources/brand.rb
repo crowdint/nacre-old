8e9cec84046054e08b87b86fc281e77a773eddd0
Steven Barragan <me@steven.mx>
2014-04-02 17:29:43 -0600

    Add brand support

---
+++ lib/nacre/api/product_service_resources/brand.rb
@@ -0,0 +1,33 @@
+require 'nacre'
+require 'json'
+require 'active_support/inflector'
+
+module Nacre
+  module API
+    class Brand < ProductServiceResource
+      FIELDS = [
+        :id,
+        :name,
+        :description
+      ]
+
+      def self.fields
+        FIELDS.map(&:to_s).map(&:underscore).map(&:to_sym)
+      end
+
+      fields.each do |attr|
+        attr_accessor attr.to_s.underscore.to_sym
+      end
+
+      def initialize(values = nil)
+        load_values(values) unless values.nil? || values.empty?
+      end
+
+      private
+
+      def self.url
+        service_url + "/brand"
+      end
+    end
+  end
+end
