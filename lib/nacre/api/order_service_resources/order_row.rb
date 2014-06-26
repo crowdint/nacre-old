3485a372c4e69082c5e73f5a61be0668e869d76b
Steven Barragán <me@steven.mx>
2014-06-26 16:17:38 -0500

    Add content-type: json

--- lib/nacre/api/order_service_resources/order_row.rb
+++ lib/nacre/api/order_service_resources/order_row.rb
@@ -8,7 +8,7 @@ module Nacre
 
       def self.create(order_id, data)
         @order_id = order_id
-        response = connection.post(url, {}, data.to_json)
+        response = connection.post(url, {"content-type"=>"application/json"}, data.to_json)
         json_response = JSON.parse(response.body)
         { id: json_response['response'] }
       end
